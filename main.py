2fc8ed2f2e1836d6961d5948956636ad4a3ff8d4
yourlucky <yoonchae.na@gmail.com>
2024-07-06 17:27:57 -0700

    model I/O

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 import tiktoken
 
 print(tiktoken)
+
+examples = [
+            {"question": "What do you know about France?", 
+            "answer": """
+                Here is what I know: ",
+                Capital: Paris ",
+                Language: French ",
+                Food: Wine and Cheese ",
+                Currency: Euro ",
+                """,
+            },
+            {
+            "question": "What do you know about Italy?",
+            "answer": """
+                I know this: ",
+                Capital: Rome ",
+                Language: Italian ",
+                Food: Pizza and Pasta ",
+                Currency: Euro ",
+                """,
+            },
+            { 
+            "question": "What do you know about Greece?",
+                "answer": """
+                I know this: ",
+                Capital: Athens ",
+                Language: Greek ",
+                Food: Souvlaki and Feta Cheese ",
+                Currency: Euro ",
+                """,
+            },
+]
